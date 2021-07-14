52dced10a21a39b646a6004e1d535ba29093cdf3
Hyunji KIM <hjikim01@gmail.com>
2021-07-14 15:38:07 +0900

    뭔가 연결을 하긴했는데 그냥 처음 만들때부터 연결해놓고 만드는게 훨씬 편하다.

---
+++ read2.R
@@ -0,0 +1,8 @@
+#이건 Git에 연결안된 프로젝트를 다시 Git에 연결하는 방법
+#Git bash를 연다.
+#cd  /F/hjikim/R_data_basic/unlinking 입력해서 경로생성
+#git init 입력하고 기다리면 master라고 나온다.
+#이제 Github로 이동해서 repository에 새로만드는데 R 프로젝트랑 이름 똑같이 만들기
+#git bash에서 push가 error 나와서 그냥 git hub에 프로젝트 파일 업로드 했더니 바로 git이랑 연결됐는데..
+#아니! commit 작성했는데 psuh 안된다
+#아, 새프로젝트를 Git에 연결해서 만들어야 완전 연결되는건가?
